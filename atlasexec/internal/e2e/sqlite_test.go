6916deccef06fb9a2788798d4a8594669e277cca
Giau. Tran Minh <hello@giautm.dev>
2024-01-12 15:08:41 +0700

    atlasexec: added end-to-end testing

---
+++ atlasexec/internal/e2e/sqlite_test.go
@@ -0,0 +1,69 @@
+package e2etest
+
+import (
+	"context"
+	"testing"
+
+	"ariga.io/atlas-go-sdk/atlasexec"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_SQLite(t *testing.T) {
+	runTestWithVersions(t, []string{"latest"}, "versioned-basic", func(t *testing.T, ver *atlasexec.Version, c *atlasexec.Client) {
+		url := "sqlite://file.db?_fk=1"
+		ctx := context.Background()
+		s, err := c.Status(ctx, &atlasexec.MigrateStatusParams{
+			URL: url,
+			Env: "local",
+		})
+		require.NoError(t, err)
+		require.Equal(t, 1, len(s.Pending))
+		require.Equal(t, "20240112070806", s.Pending[0].Version)
+
+		r, err := c.MigrateApply(ctx, &atlasexec.MigrateApplyParams{
+			URL: url,
+			Env: "local",
+		})
+		require.NoError(t, err)
+		require.Equal(t, 1, len(r.Applied), "Should be one migration applied")
+		require.Equal(t, "20240112070806", r.Applied[0].Version, "Should be the correct migration applied")
+
+		// Apply again, should be a no-op.
+		r, err = c.MigrateApply(ctx, &atlasexec.MigrateApplyParams{
+			URL: url,
+			Env: "local",
+		})
+		require.NoError(t, err, "Should be no error")
+		require.Equal(t, 0, len(r.Applied), "Should be no migrations applied")
+	})
+}
+
+func Test_PostgreSQL(t *testing.T) {
+	runTestWithVersions(t, []string{"latest"}, "versioned-basic", func(t *testing.T, ver *atlasexec.Version, c *atlasexec.Client) {
+		url := "postgres://postgres:pass@localhost:5432/postgres?search_path=public&sslmode=disable"
+		ctx := context.Background()
+		s, err := c.Status(ctx, &atlasexec.MigrateStatusParams{
+			URL: url,
+			Env: "local",
+		})
+		require.NoError(t, err)
+		require.Equal(t, 1, len(s.Pending))
+		require.Equal(t, "20240112070806", s.Pending[0].Version)
+
+		r, err := c.MigrateApply(ctx, &atlasexec.MigrateApplyParams{
+			URL: url,
+			Env: "local",
+		})
+		require.NoError(t, err)
+		require.Equal(t, 1, len(r.Applied), "Should be one migration applied")
+		require.Equal(t, "20240112070806", r.Applied[0].Version, "Should be the correct migration applied")
+
+		// Apply again, should be a no-op.
+		r, err = c.MigrateApply(ctx, &atlasexec.MigrateApplyParams{
+			URL: url,
+			Env: "local",
+		})
+		require.NoError(t, err, "Should be no error")
+		require.Equal(t, 0, len(r.Applied), "Should be no migrations applied")
+	})
+}
