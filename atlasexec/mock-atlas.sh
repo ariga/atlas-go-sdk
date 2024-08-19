2344c8e5e68adc8233defe1dfcec2ac87dbde9ea
Giau. Tran Minh <hello@giautm.dev>
2024-08-19 09:14:56 +0700

    atlasexec: mock-atlas with stdout/stderr and arguments check

--- atlasexec/mock-atlas.sh
+++ atlasexec/mock-atlas.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-TEST_ATLAS_VERSION="${TEST_ATLAS_VERSION:-v1.2.3}"
+if [[ "$TEST_ARGS" != "$@" ]]; then
+  >&2 echo "Receive unexpected args: $@"
+  exit 1
+fi
 
-echo "atlas version $TEST_ATLAS_VERSION"
+if [[ "$TEST_STDOUT" != "" ]]; then
+  echo $TEST_STDOUT
+  exit 0
+fi
+
+TEST_STDERR="${TEST_STDERR:-Missing stderr either stdout input for the test}"
+echo $TEST_STDERR
+exit 1
