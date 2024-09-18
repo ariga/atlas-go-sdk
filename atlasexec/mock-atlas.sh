5d10c687cbe16e3d420c217e33a5fe9b6e1b8a98
Giau. Tran Minh <12751435+giautm@users.noreply.github.com>
2024-09-19 00:04:21 +0700

    atlasexec: add `schema/clean` command (#94)

--- atlasexec/mock-atlas.sh
+++ atlasexec/mock-atlas.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# TEST_BATCH provide the directory containts all
+# outputs for multiple runs. The path should be absolulate
+# or related to current working directory.
+if [[ "$TEST_BATCH" != "" ]]; then
+  COUNTER_FILE=$TEST_BATCH/counter
+  COUNTER=$(cat $COUNTER_FILE 2>/dev/null)
+  COUNTER=$((COUNTER+1))
+  DIR_CUR="$TEST_BATCH/$COUNTER"
+  if [ ! -d "$DIR_CUR" ]; then
+    >&2 echo -n "$DIR_CUR does not exist, quitting..."
+    exit 1
+  fi
+  # Save counter for the next runs
+  echo -n $COUNTER > $COUNTER_FILE
+  if [ -f "$DIR_CUR/args" ]; then
+    TEST_ARGS=$(cat $DIR_CUR/args)
+  fi
+  if [ -f "$DIR_CUR/stderr" ]; then
+    TEST_STDERR=$(cat $DIR_CUR/stderr)
+  fi
+  if [ -f "$DIR_CUR/stdout" ]; then
+    TEST_STDOUT=$(cat $DIR_CUR/stdout)
+  fi
+fi
+
 if [[ "$TEST_ARGS" != "$@" ]]; then
   >&2 echo "Receive unexpected args: $@"
   exit 1
